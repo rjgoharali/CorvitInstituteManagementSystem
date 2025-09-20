82aec6bceb733a5f3f244ccd07fdaa7cfdf8a05c
Raja Gohar <106471331+rjgoharali@users.noreply.github.com>
2025-09-20 11:46:17 +0500

    Create Header.tsx

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ backgroundColor: '#333', color: 'white', padding: '1rem', textAlign: 'center' }}>
+      <h1>Education Platform</h1>
+    </header>
+  )
+}
+
+export default Header
